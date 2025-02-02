779aa94607d116bf2a6257b4118f1051eb519579
Faizan <haque.faizan@gmail.com>
2025-02-01 16:20:08 -0800

    added bottom bar navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
     <View style={styles.container}>
 
       <Text style={styles.text}>
-        Hello World! edited
+        Hello World!
       </Text>
 
       <Link href='/about' style={styles.button}>
