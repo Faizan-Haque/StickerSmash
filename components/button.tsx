792e411ad2d0ce1ac63fda99523ed73945bc68b5
Faizan <haque.faizan@gmail.com>
2025-02-03 17:07:04 -0800

    init button.tsx

--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,5 @@
 import { View, StyleSheet } from "react-native";
-import { Image } from 'expo-image';
+import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png')
 
@@ -8,7 +8,7 @@ export default function Index() {
     <View style={styles.container}>
 
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer imgSource={PlaceholderImage} />
       </View>
 
     </View>
@@ -25,17 +25,5 @@ const styles = StyleSheet.create({
 
   imageContainer: {
     flex: 1,
-  },
-
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
-  
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
   },
 })
