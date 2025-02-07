b53b2c5dbac2d29a21dd43c548fdbb7811ce35e3
Faizan <haque.faizan@gmail.com>
2025-02-06 17:07:30 -0800

    button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
 import { View, StyleSheet } from "react-native";
+
+import Button from "@/components/Button";
 import ImageViewer from '@/components/ImageViewer';
 
 const PlaceholderImage = require('@/assets/images/background-image.png')
@@ -9,6 +11,11 @@ export default function Index() {
 
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+
+      <View style = {styles.footerContainer}>
+        <Button label='Choose a photo' />
+        <Button label='Use this photo' />
       </View>
 
     </View>
@@ -25,5 +32,10 @@ const styles = StyleSheet.create({
 
   imageContainer: {
     flex: 1,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 })
